8485bab357f216e1b6c61fed16acabe78142ad2c
Nak14 <nickkraut@gmail.com>
2022-05-05 10:08:49 -0700

    Complete getTheTitles project on Odin
    this project served as a refresher on objects, arrays,
    and accessing items there in. Of particular help was this
    resource:
    https://stackoverflow.com/questions/11922383/how-can-i-access-and-process-nested-objects-arrays-or-json

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,20 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+const getTheTitles = function(books) {
+    let titles = [];
+    for (let i = 0; i < books.length; i++){
+        titles.push(books[i].title);
+    
+    }return titles;
 };
 
 // Do not edit below this line
