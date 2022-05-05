f6fd1528d98e275c63291f78bf68c075477b4cf4
Nak14 <nickkraut@gmail.com>
2022-05-05 11:37:37 -0700

    Complete findTheOldest assignment
    great practice with arrays, loops, and objects.
     See note on prior commit for how to reference specific
     objects and items contained therein

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,13 +1,3 @@
-const books = [
-    {
-      title: 'Book',
-      author: 'Name'
-    },
-    {
-      title: 'Book2',
-      author: 'Name2'
-    }
-  ]
 
 const getTheTitles = function(books) {
     let titles = [];
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,53 @@
-const findTheOldest = function() {
+let thisYear = new Date().getFullYear()
 
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+const findAge = function(people) {
+    //find age at death or currently of each person (Death minus Birth || Today minus birth)
+    for (let i = 0; i < people.length; i++){
+        
+        //if person is still living, use today's date instead of death and recalculate
+        let findAge = (people[i].yearOfDeath - people[i].yearOfBirth);
+        if (Number.isFinite(findAge) == false) {
+            let age = thisYear - people[i].yearOfBirth;
+            people[i].age = age;
+        }else  {       
+        //Add age to people object
+        people[i].age = findAge;}
+        
+}
+return people;
+}
+
+const findTheOldest = function(people) {
+    people = findAge(people);
+    
+    //return array of person with greatest age via sorting of age items
+    let longestLife = people[0];
+    for (let i = 1; i < people.length; i++){
+        if (people[i].age > people[i-1].age){
+            longestLife = people[i];
+        }
+        console.log(people);
+        return longestLife;
+    }
+    
+    
 };
 
 // Do not edit below this line
