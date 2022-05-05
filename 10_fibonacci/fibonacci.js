58d39ea9aca9c885cb11e4b7520b21e045fd5547
Nak14 <nickkraut@gmail.com>
2022-05-04 20:34:17 -0700

    Submit fibonacci project
    Great example of array manipulation, booleans, and a for loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    //pass the function a number (or string converted to number)
+    num = +num;
+    let array = [1,1];
+    if (num <= 0){
+        return 'OOPS';
+    }
+    else if(true == (num == 1 || num == 2)){
+        return 1;}
+    
+    //find which number/place in the fibonacci sequence corresponds to given number
+    for (i = 1; i < num - 1; i++){
+        array.push((array[i]+array[i-1]))
+        console.log(array);
+    }return array[num-1];
 };
 
 // Do not edit below this line
